7314c4dd75f1371c3780b6948cfc5bd03cfff73f
Theeruth Borisuth <pwnimatorz@hotmail.com>
2018-01-18 10:07:19 +0700

    edited the GameSolver into a correct way and edited README.md

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -1,37 +1,36 @@
 /**
- * This class will solve the MastGame by itself. It will narrowing the range of the min and max until it get its answer
+ * This class will solve the MastGame by itself. It will 
  * @author Theeruth Borisuth.
  *
  */
 public class GameSolver extends NumberGame {
 	/**
 	 * Initialize the GameSolver class.
-	 * @param game
-	 * @return guess number
+	 * @param game : base class from NumberGame.
+	 * @return guess : return the correct number.
 	 */
 	public int play(NumberGame game){
-		return solve(game, game.getUpperBound(), game.getUpperBound()/2);
-	}
-	/**
-	 * This method will be the key for Solving MastGame.
-	 * @param game : this use to call base class called number game.
-	 * @param upperBound : It is a maximum value of the number that we can guess
-	 * @param answer : this variable use to return the correct answer.
-	 * @return answer
-	 */
-	private int solve(NumberGame game, int upperBound, int answer){
-		game.guess(answer);
-		if (game.getMessage().contains("too small")){
-			if ((upperBound - answer) == 1){
-				game.guess(answer + 1);
-				return answer + 1;
-			} 
-			else return solve(game, upperBound, answer + (upperBound - answer) / 2);
-			
-		} else if (game.getMessage().contains("too large")) return solve(game, answer - 1 , answer/2);
-		
-		System.out.println("Mission completed. The number is "+answer);
+		int max = game.getUpperBound();
+		int min = 1;
+		int guess = min + (max - min)/2 ; 
+		while (!game.guess(guess)){
+			game.guess(guess);
+			//get message from the number game class.
+			String hint = game.getMessage();
+			//If the guess number is too large it will set the max into the guess number.
+			if(hint.toLowerCase().contains("large")){
+				max = guess + 1; 
+				guess = min + (max - min)/2;
+			}	
+			//If the guess number is too large it will set the max into the guess number.
+			if(hint.toLowerCase().contains("small")){
+				min = guess - 1;
+				guess = min + (max - min)/2;
+			}
+		 }
+		System.out.println("Mission completed. The number is "+guess);
 		System.out.println("I have tried "+game.getCount()+" times.");
-		return answer;
+		return guess;
 	}
+
 }
