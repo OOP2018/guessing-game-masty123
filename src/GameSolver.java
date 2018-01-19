e55abe93c47d13ac8bbb09d76f70e3d028fa8c02
Theeruth Borisuth <pwnimatorz@hotmail.com>
2018-01-19 10:45:54 +0700

    Fixed the upperBound problem

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -24,6 +24,10 @@ public class GameSolver extends NumberGame {
 			}	
 			//If the guess number is too large it will set the max into the guess number.
 			if(hint.toLowerCase().contains("too small")){
+				if ((max - guess) == 1){
+				game.guess(guess+1);
+				return guess + 1  ;
+				}
 				min = guess - 1;
 				guess = min + (max - min)/2;
 			}
